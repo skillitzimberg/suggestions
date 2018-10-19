0d7026f087a9b61bc6712b87d62ebda75464d069
Scott Bergler <example@example.com>
2018-10-19 14:23:06 -0700

    add all questions and begin building logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    var favTopic = $("#topic").val();
-    var myHabits = $("#habits").val();
+    var devField = $("#field").val();
+    var langaugePopularity = $("#popularity").val();
+    var targetPlatform = $("#platform").val();
     var bizSize = $("#business").val();
-    console.log(favTopic);
-    console.log(myHabits);
-    console.log(bizSize);
+    var favFruit = $("#fruit").val();
+
+    // console.log(devField);
+    // console.log(langaugePopularity);
+    // console.log(targetPlatform);
+    // console.log(bizSize);
+    // console.log(favFruit);
+
+    if (devField && langaugePopularity && targetPlatform && bizSize && favFruit) {
+      if () {
+      } else if (true) {
+
+      } else if (true) {
+
+      } else if (true) {
+
+      }
+
+    } else {
+      $("h3#fail").removeClass("hide");
+    }
 
     event.preventDefault();
   });
