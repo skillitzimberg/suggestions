d3e4f7b9dd38c9d8f591597215cf73486029811e
Scott Bergler <example@example.com>
2018-10-19 12:06:46 -0700

    add select boxes & function to get values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-  console.log("I am alive!");
+  $("form#survey").submit(function(event) {
+    var favTopic = $("#topic").val();
+    var myHabits = $("#habits").val();
+    var bizSize = $("#business").val();
+    console.log(favTopic);
+    console.log(myHabits);
+    console.log(bizSize);
 
+    event.preventDefault();
+  });
 });
