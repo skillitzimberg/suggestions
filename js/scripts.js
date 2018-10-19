fe2e79002f499c4f7533df6e50d8e4ad30f0fba2
Scott Bergler <example@example.com>
2018-10-19 16:55:07 -0700

    add logic to branch refactordisplay. Not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
+
     var devField = $("#field").val();
     var langaugePopularity = $("#popularity").val();
     var targetPlatform = $("#platform").val();
     var bizSize = $("#business").val();
     var favFruit = $("#fruit").val();
-    var suggestion;
-    
+
+    var cSharp = "C#/.NET";
+    var java = "Java/Android";
+    var php = "PHP/Drupal";
+    var ruby = "Ruby/Rails";
+    var css = "CSS/Design";
+
+    var suggestion = "We suggest that you look further into these langauges:";
+    var allOptions = "The world is your oyster! Do it all!";
+    var fail = "Please fill out the form completely.";
+
     console.log(devField);
     console.log(langaugePopularity);
     console.log(targetPlatform);
     console.log(bizSize);
     console.log(favFruit);
-    debugger;
-    if (devField && langaugePopularity && targetPlatform && bizSize && favFruit) {
+    console.log(suggestion);
+    console.log(allOptions);
 
+
+    if (devField && langaugePopularity && targetPlatform && bizSize && favFruit) {
+      debugger;
       if (devField === "web" && targetPlatform === "ios" || bizSize === "freelance" && favFruit === "golfball") {
         $("h2#suggest").removeClass("hide");
         $("h3#ruby").removeClass("hide");
@@ -41,6 +54,7 @@ $(document).ready(function() {
     } else {
       $("h3#fail").removeClass("hide");
     }
+    $("form#survey").reset();
 
     event.preventDefault();
   });
