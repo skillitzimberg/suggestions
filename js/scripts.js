1e7708b349876e843de31fb79a1ca812759b35a6
Scott Bergler <commandinghands@gmail.com>
2018-10-21 07:18:44 -0700

    change layout of form and display.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
 
-    $("h2#suggest").addClass("hide");
-    $("h2#listAll").addClass("hide");
-    $("h3#cSharp").addClass("hide");
-    $("h3#java").addClass("hide");
-    $("h3#php").addClass("hide");
-    $("h3#ruby").addClass("hide");
-    $("h3#css").addClass("hide");
-    $("h3#fail").addClass("hide");
+    $("h3#suggest").addClass("hide");
+    $("h3#listAll").addClass("hide");
+    $("h4#cSharp").addClass("hide");
+    $("h4#java").addClass("hide");
+    $("h4#php").addClass("hide");
+    $("h4#ruby").addClass("hide");
+    $("h4#css").addClass("hide");
+    $("h4#fail").addClass("hide");
 
     var devField = $("#field").val();
     var langaugePopularity = $("#popularity").val();
@@ -19,30 +19,30 @@ $(document).ready(function() {
 
     if (devField && langaugePopularity && targetPlatform && bizSize && favFruit) {
       if (devField === "web" && targetPlatform === "ios" || bizSize === "freelance" && favFruit === "golfball") {
-        $("h2#suggest").removeClass("hide");
-        $("h3#ruby").removeClass("hide");
-        $("h3#css").removeClass("hide");
+        $("h3#suggest").removeClass("hide");
+        $("h4#ruby").removeClass("hide");
+        $("h4#css").removeClass("hide");
       } else if (bizSize === "large" && targetPlatform === "microsoft") {
-        $("h2#suggest").removeClass("hide");
-        $("h3#cSharp").removeClass("hide");
+        $("h3#suggest").removeClass("hide");
+        $("h4#cSharp").removeClass("hide");
       } else if (favFruit === "goji" || langaugePopularity === "dontCareAboutPopularity") {
-        $("h2#suggest").removeClass("hide");
-        $("h3#java").removeClass("hide");
-        $("h3#php").removeClass("hide");
+        $("h3#suggest").removeClass("hide");
+        $("h4#java").removeClass("hide");
+        $("h4#php").removeClass("hide");
       } else if (devField === "android" && bizSize === "medium") {
-        $("h2#suggest").removeClass("hide");
-        $("h3#java").removeClass("hide");
+        $("h3#suggest").removeClass("hide");
+        $("h4#java").removeClass("hide");
       } else {
         $("#listAll").removeClass("hide");
-        $("h3#cSharp").removeClass("hide");
-        $("h3#java").removeClass("hide");
-        $("h3#php").removeClass("hide");
-        $("h3#ruby").removeClass("hide");
-        $("h3#css").removeClass("hide");
+        $("h4#cSharp").removeClass("hide");
+        $("h4#java").removeClass("hide");
+        $("h4#php").removeClass("hide");
+        $("h4#ruby").removeClass("hide");
+        $("h4#css").removeClass("hide");
       }
 
     } else {
-      $("h3#fail").removeClass("hide");
+      $("h4#fail").removeClass("hide");
     }
 
   });
